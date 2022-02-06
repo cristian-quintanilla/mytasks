502768ecce10af4f8eba5cbd417b1e9c5b291ff9
Cristian Quintanilla <cristiancbtis130@gmail.com>
2022-02-05 20:47:21 -0600

    Login page styles

---
+++ pages/login.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/no-unescaped-entities */
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { BsGoogle, BsFacebook, BsTwitter } from 'react-icons/bs';
+
+const LoginPage = () => {
+	return (
+		<>
+			<Head>
+				<title>MyTasks - Login</title>
+				{/* <link rel='shortcut icon' href='/static/img/web_icon.ico' /> */}
+			</Head>
+
+			<div className='login'>
+				<form className='login__form'>
+					<h1>Login</h1>
+
+					<section className='login__social'>
+						<button className='login__social-button'>
+							<BsGoogle />
+						</button>
+
+						<button className='login__social-button'>
+							<BsFacebook />
+						</button>
+
+						<button className='login__social-button'>
+							<BsTwitter />
+						</button>
+					</section>
+
+					<p className='login__text'>or use your account</p>
+
+					<section className='login__form-group'>
+						<label htmlFor='email' className='login__form-group-label'>
+							Email:
+						</label>
+
+						<input type='email' className='login__form-group-input' id='email' />
+					</section>
+
+					<section className='login__form-group'>
+						<label htmlFor='password' className='login__form-group-label'>
+							Password:
+						</label>
+
+						<input type='password' className='login__form-group-input' id='password' />
+					</section>
+
+					<button className='login__form-submit'>
+						Login
+					</button>
+				</form>
+
+				<section className='login__register'>
+					<h1>Hello, Friend!</h1>
+					<p>
+						You don't have an account? You can create one here:
+					</p>
+
+					<Link href='/register'>
+						<a>Register</a>
+					</Link>
+				</section>
+			</div>
+		</>
+	);
+}
+
+export default LoginPage;
---
+++ pages/register.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+import Head from 'next/head';
+import Link from 'next/link';
+
+const LoginPage = () => {
+	return (
+		<>
+			<Head>
+				<title>MyTasks - Register</title>
+				{/* <link rel='shortcut icon' href='/static/img/web_icon.ico' /> */}
+			</Head>
+
+			<div className='register'>
+				<h1>Register</h1>
+			</div>
+		</>
+	);
+}
+
+export default LoginPage;
